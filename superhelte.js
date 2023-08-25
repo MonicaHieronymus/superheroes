2d0ad8fbe1c507e67ea3a9bdb21fbba9289044a6
Monica <moha0001@stud.kea.dk>
2023-08-25 14:45:54 +0200

    copy qs

--- superhelte.js
+++ superhelte.js
@@ -13,6 +13,18 @@ function showSuperhero(superhero) {
   const template = document.querySelector("template").content;
   const copy = template.cloneNode(true);
 
+  copy.querySelector("h2").textContent = superhero.alias;
+  copy.querySelector("p.realname").textContent = superhero.realname;
+  copy.querySelector("p.alias").textContent = superhero.alias;
+  copy.querySelector("p.origin").textContent = superhero.origin;
+  copy.querySelector("p.weakness").textContent = superhero.weakness;
+  copy.querySelector("p.active").textContent = superhero.active;
+  copy.querySelector("p.evil").textContent = superhero.evil;
+  copy.querySelector("p.height").textContent = superhero.height;
+  copy.querySelector("p.powers").textContent = superhero.powers;
+
+  copy.querySelector("img").src = superhero.image;
+
   const parent = document.querySelector(".grid");
   parent.appendChild(copy);
 }
